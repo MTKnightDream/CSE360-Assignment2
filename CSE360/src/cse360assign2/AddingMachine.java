afa3fd1f600cd799fceaa452e53b7c9801c6878a
Maximus <tkieu2@asu.edu>
2019-10-08 14:26:38 -0700

    Initial class AddingMachine

---
+++ CSE360/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,54 @@
+/****************************************************************
+ * Name: Maximus Kieu
+ * ID: 1212535049
+ * Assignment 1
+ * Description: This class calculute the number of machines
+*****************************************************************/
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	/*
+	 * This is a constructor that set total to 0
+	 */
+	public AddingMachine () {
+		total = 0;    // not needed - included for clarity
+	}
+	
+	/*
+	 * This method is return the total number 
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	 * This method will add the parameter "value" to total
+	 * @value the value that will use to add
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/*
+	 * This method will subtract the parameter "value" to total
+	 * @value the value that will use to subtract
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/*
+	 * This method will print out a string 
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/* 
+	 * This method will clear all the numbers
+	 */
+	public void clear() {
+}
