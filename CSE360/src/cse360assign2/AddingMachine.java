261e1504c1a0b3ce1fa92539a56107c061994bd3
Maximus <tkieu2@asu.edu>
2019-10-08 14:56:27 -0700

    Give the definition for each methods and edit the comment

--- CSE360/src/cse360assign2/AddingMachine.java
+++ CSE360/src/cse360assign2/AddingMachine.java
@@ -2,13 +2,14 @@
  * Name: Maximus Kieu
  * ID: 1212535049
  * Assignment 1
- * Description: This class calculute the number of machines
+ * Description: This class calculate the total number 
 *****************************************************************/
 package cse360assign2;
 
 public class AddingMachine {
 	
 	private int total;
+	private String keepTrack = "";
 	
 	/*
 	 * This is a constructor that set total to 0
@@ -21,34 +22,50 @@ public class AddingMachine {
 	 * This method is return the total number 
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/*
 	 * This method will add the parameter "value" to total
+	 * each time the method is call
+	 * the string "+ 'value' " will be add to the string keepTrack
+	 * to keep track of the history of the calculation
 	 * @value the value that will use to add
 	 */
 	public void add (int value) {
+		total += value;
 		
+		keepTrack += "+ ";
+		keepTrack += Integer.toString(value);
+		keepTrack += " ";
 	}
 	
 	/*
 	 * This method will subtract the parameter "value" to total
+	 * each time the method is call
+	 * the string "- 'value' " will be add to the string keepTrack
+	 * to keep track of the history of the calculation
 	 * @value the value that will use to subtract
 	 */
 	public void subtract (int value) {
-		
+		total -= value;
+		keepTrack += "- ";
+		keepTrack += Integer.toString(value);
+		keepTrack += " ";
 	}
 	
 	/*
 	 * This method will print out a string 
+	 * with the initial value is 0 with the history of calculation
 	 */
 	public String toString () {
-		return "";
+		return "0 " + keepTrack;
 	}
 	
 	/* 
 	 * This method will clear all the numbers
 	 */
 	public void clear() {
+		
+	}
 }
